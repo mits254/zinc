728405e82d1f3891bdc9174ef06de325da0f27c8
Mitali Mittal <mits254@gmail.com>
2018-10-25 10:49:27 -0500

    part 1

--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -5,12 +5,43 @@
 const Zinc = {};
 
 (() => {
-    function renderComponent(element, content, userData) {
-        console.log(element, content); // eslint-disable-line no-console
+    function renderComponent(element, content, userDATA) {
+        //console.log(element, content); // eslint-disable-line no-console
+        //console.log(userDATA)
+        let str = content.replace(/\{\{\s*(.*?)\s*\}\}/g,(match,p1)=> p1.split('.').reduce(function(acc, current) {   
+            return acc[current];
+          },userDATA)
+          ); 
+     
+         let ul = document.getElementById('z-user-list');
+         ul.insertAdjacentHTML('beforeend',str);
+
     }
 
     function init() {
-        renderComponent('user-item', 'user', userData);
+        let user = document.getElementsByClassName('user');
+        let element = document.querySelector('user-item') ; 
+        const userData = {
+            picture: {
+                thumbnail: 'https://f4.bcbits.com/img/0001142378_10.jpg'
+            },
+            name: {
+                first: 'Jack',
+                last: 'Burton'
+            },
+            location: {
+                city: 'San Francisco',
+                state: 'CA'
+            },
+            email: 'jack.burton@example.com'
+        };
+
+        fetch('user.html')
+    .then((resp) => resp.text())
+    .then(function (user) {  
+        //console.log(user);  
+        renderComponent(element, user, userData);
+    })
     }
 
     document.addEventListener('DOMContentLoaded', init);
