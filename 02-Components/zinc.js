71c7e9dcd0529af006e0068e95ef5f80a69ce96f
Mitali Mittal <mits254@gmail.com>
2018-10-29 10:46:52 -0500

    part - 2

--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -1,17 +1,43 @@
-'use strict';
-
-/* eslint-env browser */
-
 const Zinc = {};
-
 (() => {
-    function renderComponent(element, content, userData) {
-        console.log(element, content); // eslint-disable-line no-console
+    document.addEventListener('DOMContentLoaded', init);
+    
+    function renderComponent(templateFile, dataObject) {
+        return fetch(`${templateFile}.html`)
+            .then(res => res.text())
+            .then((template) => {
+                return template.replace(/\{\{\s*(.*?)\s*\}\}/g, (match, p1) => p1.split('.').reduce(function (acc, current) {
+                    acc + current;
+                    return acc[current];
+                }, dataObject)
+                );
+            })
+    }
+
+    Zinc.registerComponent = function (elementName, templateFile, dataObject) {
+        let element = document.querySelector(elementName);
+        renderComponent(templateFile, dataObject)
+            .then((result) => {
+                console.log(result);
+                element.insertAdjacentHTML('beforeend', result);
+            })
     }
 
     function init() {
-        renderComponent('user-item', 'user', userData);
+        fetch('https://randomuser.me/api/?results=5')
+            .then(res => res.json())
+            .then(res=> {
+                 res.results.forEach(result => {
+                    Zinc.registerComponent('user-item', 'user', result);
+                 });
+                    
+                 
+            });
+    
+
+
+
     }
 
-    document.addEventListener('DOMContentLoaded', init);
+    
 })();
