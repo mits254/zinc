bd1963e675a6ec7b5b941b7c6f360963ec6c8500
Mitali Mittal <mits254@gmail.com>
2018-10-30 10:36:50 -0500

    part

--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -1,41 +1,63 @@
 const Zinc = {};
 (() => {
+    Zinc._components = {};
     document.addEventListener('DOMContentLoaded', init);
-    
-    function renderComponent(templateFile, dataObject) {
+   
+    function renderTemplate(templateFile, dataObject) {
         return fetch(`${templateFile}.html`)
             .then(res => res.text())
             .then((template) => {
                 return template.replace(/\{\{\s*(.*?)\s*\}\}/g, (match, p1) => p1.split('.').reduce(function (acc, current) {
-                    acc + current;
                     return acc[current];
                 }, dataObject)
                 );
             })
     }
 
-    Zinc.registerComponent = function (elementName, templateFile, dataObject) {
-        let element = document.querySelector(elementName);
-        renderComponent(templateFile, dataObject)
-            .then((result) => {
-                console.log(result);
-                element.insertAdjacentHTML('beforeend', result);
-            })
-    }
+    Zinc.registerComponent = function (elementName, templateFile, dataObject,controller) {
+        Zinc._components[elementName] = {
+         elementName, 
+         templateFile,
+         dataObject,
+         controller
+        }
+        let component = Zinc._components[elementName];
+        let elements = document.querySelectorAll(elementName);
 
-    function init() {
-        fetch('https://randomuser.me/api/?results=5')
-            .then(res => res.json())
-            .then(res=> {
-                 res.results.forEach(result => {
-                    Zinc.registerComponent('user-item', 'user', result);
-                 });
-                    
-                 
-            });
+        //console.log(elements);
+        for(let i= 0; i< elements.length; i++){
+            renderTemplate(templateFile, dataObject)
+            .then((result) => {
+               // console.log(result);
+                let parser = new DOMParser();
+                  let data = parser.parseFromString(result,'text/html');
+                  
+                  const el = elements[i].insertAdjacentElement('beforeend', data.firstChild.children[1].firstChild);
+                  //console.log(el);
+                  el.$state = {};
+                    if (component.controller) {
+                        el.$controller = component.controller;
+                        el.$controller();
+                    }
+                    Zinc._components[elementName].element = el;
+                // let el = dataObject.insertAdjacentHTML('beforeend',data.firstChild.children[1].firstChild);
+                // el.$state= {};
+                // if(controller) {
+                //     el.$controller = controller;
+                //     el.$controller();
+                // }
+                //elements.insertAdjacentHTML('beforeend', result);
+            })
+        
+                // let ela = document.getElementsByClassName(templateFile)
+                // console.log(JSON.stringify(ela));
+        }
+            
+    }
     
 
-
+    function init() {
+        
 
     }
 
